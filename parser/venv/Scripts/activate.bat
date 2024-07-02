128c75884728b4b1e8a9fec242af842bdf3af225
NikitaCherskov <nikitacherskov@gmail.com>
2024-07-02 16:05:29 +0300

    Добавил основу Docker контейнера

--- parser/venv/Scripts/activate.bat
+++ parser/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\User\Desktop\PROJECT PRACTICUM\parser test\venv
+set VIRTUAL_ENV=C:\Users\User\Desktop\PROJECT PRACTICUM\hhru parser\hhru_parser\parser\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
